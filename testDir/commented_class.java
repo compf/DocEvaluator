7246c043ba3af26b6ae43e28d0103e28bcbcc811
timo.schoemaker <timo.schoemaker@hotmail.de>
2021-10-28 01:22:57 +0200

    added testDir which was updated

---
+++ testDir/commented_class.java
@@ -0,0 +1,27 @@
+import java.util;
+/**
+ * A class
+ */
+public class Main {
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    private int field;
+}
+
+public class SecondClass{
+
+    public void test(int a){
+        int gh1;
+    }
+    /**
+     * 
+     */
+    private String hallo(){
+
+    }
+}
