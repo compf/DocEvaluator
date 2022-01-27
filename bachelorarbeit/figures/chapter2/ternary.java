9d2d564bc559a23ed5a25cd7f64e8e02ed2e0e8d
timo.schoemaker <timo.schoemaker@hotmail.de>
2022-01-27 20:59:10 +0100

    restructured figures and some other improvements

---
+++ bachelorarbeit/figures/chapter2/ternary.java
@@ -0,0 +1,10 @@
+/**
+ * This method simulates a ternary operator
+ * @param condition The condition that decides which param will be returned
+ * @param ifTrue will be returned if {@code condition} is true
+ * @param ifFalse will be returned if {@code condition} is false
+ * @return either {@code ifTrue} or {@code ifFalse} depending on the condition
+ */
+Object ternary(boolean condition,Object ifTrue,Object ifFalse){
+    return condition ? ifTrue :ifFalse;
+}
