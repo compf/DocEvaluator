35a8ad3c3dd02cc91c30f7f4920385d3268dd58c
timo.schoemaker <timo.schoemaker@hotmail.de>
2022-03-07 22:25:51 +0100

    more changes in conf files and added eval output

---
+++ eval/test_quality_lowest.sh
@@ -0,0 +1,3 @@
+python3 eval_quality.py --rulesets lowest_common_denominator/ --project ~/data/uni/sem7/github_projects/argouml/src/ >../eval/argo_lowest.txt
+python3 eval_quality.py --rulesets lowest_common_denominator/ --project ~/data/uni/sem7/github_projects/logging-log4j1/src  >../eval/log4j_lowest.txt
+python3 eval_quality.py --rulesets lowest_common_denominator/ --project ~/data/uni/sem7/github_projects/aspectj.eclipse.jdt.core/org.eclipse.jdt.core  >../eval/eclipse_lowest.txt
